4e17a43663a72dbd8d2378035389e51d74b11562
soham7998 <112894790+soham7998@users.noreply.github.com>
2022-12-03 23:16:57 +0530

    Add files via upload

---
+++ demo1.py
@@ -0,0 +1,12 @@
+# print("U CAN DO IT")
+
+# num1 = int(input('Enter The First Number: '))
+# num2 = int(input('Enter The secound Number: '))
+# print(num1 + num2)
+S_name =input("Enter the name of Student: ")
+a=int(input("Enter the marks: "))
+b=int(input("Enter the pasiing marks: "))
+if a > b: 
+ print(S_name,"You are passed")
+else:
+ print(S_name,"You are failed")
