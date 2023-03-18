d0cf6b0ee2e5c622eb4b7cf73b5d6f2528b5c475
soham7998 <112894790+soham7998@users.noreply.github.com>
2023-03-18 19:58:05 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,37 @@
+# 1
+# 1 1
+# 1 2 1
+# 1 3 3 1
+# 1 4 6 4 1
+# 1 5 10 5 1
+# nCr =i!//(i-j)*j
+
+# from math import factorial
+# n = int(input("Enter the number: "))
+
+# for i in range(n):
+#     for j in range(n-i+1):
+
+#         print(end=" ")
+
+#     for j in range(i+1):
+#         print(factorial(i)//(factorial(j)*factorial(i-j)),end=" ")
+
+#     print()     
+# MODULES
+# import  multiplication 
+# print(multiplication(8,8))
+# oso =(45,55,True,"apple"),
+# oso =(45,55,True,"apple"),(45,78,88,"soham")
+# print(type(oso))
+# print(oso)
+
+
+jetha=('dharmit','soham','harry')
+i = 0 
+while i < len(jetha): 
+#  if i == 1:
+   print(jetha[1])
+   break
+
+
